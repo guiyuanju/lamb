89b6cba18953e3abdc8463266af3a7bcee335d2b
guiyuanju <guiyuanjune@outlook.com>
2025-12-17 05:33:49 +0800

    rmv comment

--- main.go
+++ main.go
@@ -482,17 +482,3 @@ func repl() {
 func main() {
 	repl()
 }
-
-// (\x.M)N == let x = N in M
-
-// let 0 = \f.\x.x in let succ = \n.\f.\x.f (n f x) in succ 0
-// (λf.(λx.(f x)))
-
-// let 0 = \f.\x.x in let succ = \n.\f.\x.f (n f x) in succ (succ 0)
-// (λf.(λx.(f (f x))))
-
-// let 0 = \f.\x.x in let succ = \n.\f.\x.f (n f x) in succ (succ (succ 0))
-// (λf.(λx.(f (f (f x)))))
-
-// let 0 = \f.\x.x in let succ = \n.\f.\x.f (n f x) in let 1 = succ 0 in let + = \m.\n.\f.\x .m f (n f x) in (+ 1 (+ 1 (+ 1 1)))
-// (λf.(λx.(f (f (f (f x))))))
